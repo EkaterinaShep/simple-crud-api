ab5ee63af46715e49ce0289f5135d0e7aa986189
Ekaterina Shepeleva <ea.shepeleva95@gmail.com>
2021-11-28 13:00:54 +0300

    fix: change hasRequiredProperties

--- simple-crud-api/src/helpers/validation/has-required-properties.mjs
+++ simple-crud-api/src/helpers/validation/has-required-properties.mjs
@@ -2,7 +2,7 @@ function hasRequiredProperties(reqBody, properties) {
   const parsedBody = JSON.parse(reqBody);
 
   for (let i = 0; i < properties.length; i++) {
-    if (properties[i] in parsedBody) {
+    if (!(properties[i] in parsedBody)) {
       return false;
     }
   }
